4814c488e1c257789db804902bb874394da61182
Brian <jacklevybrsg@gmail.com>
2021-06-25 08:29:02 -0500

    add styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import './style/Badge.css'
 import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
     render() {
         return  (
-        <div>
-            <div>
+        <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt="Logo de la conferencia"/>
             </div>
-            <div>
-                <img src="" alt="Avatar"/>
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon" alt="Avatar"/>
                 <h1>Richard <br /> Kaufman</h1>
             </div>
-            <div>
+            <div className="Badge__section-info">
                 <p>Fronted Engineer</p>
                 <p>@sparragus</p>
             </div>
-            <div>
+            <div className="Badge__footer">
                 #Platziconf
             </div>
         </div>
